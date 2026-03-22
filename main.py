1351ca443cc7f078e22fb66991e3558113cc952c
nydeve <nylahgdeve@gmail.com>
2026-03-22 00:50:12 +0000

    api structure and base routes completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+#---------------uvicorn main:app --reload --host 0.0.0.0 --port 8000
+
 app = FastAPI()
 
 # mock data
@@ -58,3 +60,45 @@ def approve_chore(chore_id: int):
             chore["approved"] = True
             return {"message": f"Chore {chore_id} approved", "chore": chore}
     return {"error": "Chore not found"}
+
+@app.delete("/chores/{chore_id}")
+def delete_chore(chore_id: int):
+    for chore in chores:
+        if chore["id"] == chore_id:
+            chores.remove(chore)
+            return {"message": f"Chore {chore_id} deleted", "chore": chore}
+    return {"error": "Chore not found"}
+
+#-------------------------------------------------------------------------------------------
+
+# user routes
+
+@app.get("/users")
+def get_users():
+    return users
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    for user in users:
+        if user["id"] == user_id:
+            return user
+    return {"error": "User not found"}
+
+
+@app.post("/users")
+def create_user(user: dict):
+    users.append(user)
+    return {"message": "User created", "user": user}
+
+#---------------------------------------------------------------------------------
+
+# AUTH ROUTES
+
+@app.post ("/signup")
+def signup(user: dict):
+    users.append(user)
+    return {"message": "Signup successful", "user": user}
+
+@app.post("/login")
+def login(credentials: dict):
+    return {"message": "Login route created", "credentials": credentials}
