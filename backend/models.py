a2d1aa1b3ce56f802e9ca549a529a4b3d719229e
carmarti <carlamartin80@gmail.com>
2026-04-10 12:00:23 -0400

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,7 @@
-from sqlmodel import SQLModel, Field
-from typing import Optional
+# bridge between database and auth
+
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional, List
 from datetime import datetime
 from enum import Enum
 
@@ -15,14 +17,20 @@ class User(SQLModel, table=True):
     role: UserRole = UserRole.PARENT
     is_active: bool = True
     created_at: datetime = Field(default_factory=datetime.utcnow)
-
     parent_id: Optional[int] = Field(default=None, foreign_key="user.id")
 
-# Portia's code (Converted to SQLModel so it works with the Login system)
+    total_points: int = Field(default=0) 
+    chores: List["Chore"] = Relationship(back_populates="assigned_user")
+
 class Chore(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     title: str
-    status: str = "assigned"
+    description: Optional[str] = None
+    points_worth: int = Field(default=10)
+    status: str = Field(default="assigned") 
+    
+    user_id: Optional[int] = Field(default=None, foreign_key="user.id")
+    assigned_user: Optional[User] = Relationship(back_populates="chores")
 
 class Reward(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
